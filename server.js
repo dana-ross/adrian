c1ceb10024e09954812b623ac6b28c90088ebce0
Dave Ross <dave@davidmichaelross.com>
2017-07-19 12:17:50 -0400

    Removed unused function

--- server.js
+++ server.js
@@ -52,10 +52,6 @@ function fontType(font) {
     }
 }
 
-function fontCSS(font) {
-    return `@font-face { font-family: '${font.familyName}'; font-style: normal; font-weight: 400; src: local('Roboto'), local('Roboto-Regular'), url(https://fonts.gstatic.com/s/roboto/v16/ek4gzZ-GeXAPcSbHtCeQI_esZW2xOQ-xsNqO47m55DA.woff2) format('woff2'); unicode-range: U+0460-052F, U+20B4, U+2DE0-2DFF, U+A640-A69F;`
-}
-
 const config = readConfig('font-server.yaml')
 
 const fontDirectories = (has(config, 'global') && has(config.global, 'directories')) ? config.global.directories : []
