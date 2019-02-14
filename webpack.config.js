0e0a4b51c5023718227e560aaee0194894389bdf
Dave Ross <dave@davidmichaelross.com>
2019-02-13 22:28:58 -0500

    Webpack 3.x fixes

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
-        loaders: [
+        rules: [
             {enforce: 'post', test: /fontkit[\/\\]index.js$/, loader: "transform-loader?brfs"},
             {enforce: 'post', test: /unicode-properties[\/\\]index.js$/, loader: "transform-loader?brfs"},
             {enforce: 'post', test: /linebreak[\/\\]src[\/\\]linebreaker.js/, loader: "transform-loader?brfs"}
