5d6b6dc6ac9139751a06a2496ad2e238afa19ed5
Dana Ross <dana@danaross.dev>
2021-02-09 23:45:47 -0500

    Updated the version number

--- main.go
+++ main.go
@@ -27,11 +27,11 @@ func main() {
 
 	// Handle the --version parameter
 	if *versionParam {
-		fmt.Printf("%s\n", "2.2.3")
+		fmt.Printf("%s\n", "3.0.0")
 		os.Exit(0)
 	}
 
-	log.Println("Starting Adrian 2.2.3")
+	log.Println("Starting Adrian 3.0.0")
 	log.Println("Loading adrian.yaml")
 	var config adrianConfig.Config
 	if *configParam != "" {
